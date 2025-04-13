be1beb4f8df6ba34cd4f57a4359c10d384e32568
Doanh36 <doanhabcxyz2006@gmail.com>
2025-04-13 16:06:54 +0700

    di chuyển mượt hơn và xoay hướng

--- BTL-LTNC/src/Object/Pacman.cpp
+++ BTL-LTNC/src/Object/Pacman.cpp
@@ -7,45 +7,19 @@ Pacman::Pacman(int x, int y, std::string textureID)
       m_frame(0), m_lastFrameTime(SDL_GetTicks()) {}
 
 bool Pacman::CanMove( int newX, int newY, const int maze[MAP_HEIGHT][MAP_WIDTH] ) {
-    const int offset = TILE_SIZE / 4;  // độ lệch để kiểm tra 2 điểm
+    // Tính tile mà 4 góc Pacman chạm tới dựa trên tâm (x, y)
+    int left   = (newX - TILE_SIZE / 2) / TILE_SIZE;
+    int right  = (newX + TILE_SIZE / 2 - 1) / TILE_SIZE;
+    int top    = (newY - TILE_SIZE / 2) / TILE_SIZE;
+    int bottom = (newY + TILE_SIZE / 2 - 1) / TILE_SIZE;
 
-    // Vị trí hai điểm phía trước (tùy hướng)
-    int px1 = newX, py1 = newY;
-    int px2 = newX, py2 = newY;
-
-    switch (m_direction) {
-        case UP:
-            px1 -= offset; py1 -= TILE_SIZE / 2;
-            px2 += offset; py2 -= TILE_SIZE / 2;
-            break;
-        case DOWN:
-            px1 -= offset; py1 += TILE_SIZE / 2;
-            px2 += offset; py2 += TILE_SIZE / 2;
-            break;
-        case LEFT:
-            px1 -= TILE_SIZE / 2; py1 -= offset;
-            px2 -= TILE_SIZE / 2; py2 += offset;
-            break;
-        case RIGHT:
-            px1 += TILE_SIZE / 2; py1 -= offset;
-            px2 += TILE_SIZE / 2; py2 += offset;
-            break;
-    }
-
-    // Chuyển pixel sang tọa độ tile
-    int tileX1 = px1 / TILE_SIZE;
-    int tileY1 = py1 / TILE_SIZE;
-    int tileX2 = px2 / TILE_SIZE;
-    int tileY2 = py2 / TILE_SIZE;
-
-    // Kiểm tra nằm trong bản đồ
-    if (tileX1 < 0 || tileX1 >= MAP_WIDTH || tileY1 < 0 || tileY1 >= MAP_HEIGHT ||
-        tileX2 < 0 || tileX2 >= MAP_WIDTH || tileY2 < 0 || tileY2 >= MAP_HEIGHT) {
+    if (left < 0 || right >= MAP_WIDTH || top < 0 || bottom >= MAP_HEIGHT)
         return false;
-    }
 
-    // Trả về true nếu cả hai điểm không va vào tường
-    return (maze[tileY1][tileX1] == 0 && maze[tileY2][tileX2] == 0);
+    return (maze[top][left] == 0 &&
+            maze[top][right] == 0 &&
+            maze[bottom][left] == 0 &&
+            maze[bottom][right] == 0);
 
 }
 
@@ -56,41 +30,36 @@ void Pacman::Update( const int maze[MAP_HEIGHT][MAP_WIDTH]) {
         m_lastFrameTime = SDL_GetTicks();
     }
 
-    // Gần trung tâm tile chưa?
-    int tileCenterX = ((m_x + TILE_SIZE / 2) / TILE_SIZE) * TILE_SIZE;
-    int tileCenterY = ((m_y + TILE_SIZE / 2) / TILE_SIZE) * TILE_SIZE;
 
-    bool nearCenterX = abs(m_x - tileCenterX) < 2;
-    bool nearCenterY = abs(m_y - tileCenterY) < 2;
+    // Rẽ tự động nếu đến giữa ô
+    bool isAlignedX = (m_x % TILE_SIZE == TILE_SIZE / 2);
+    bool isAlignedY = (m_y % TILE_SIZE == TILE_SIZE / 2);
 
-    // Nếu gần tâm và có hướng mới, thử rẽ
-    if (nearCenterX && nearCenterY) {
-        int tryX = m_x;
-        int tryY = m_y;
+    if (isAlignedX && isAlignedY && nextDir != currentDir) {
+        int testX = m_x, testY = m_y;
 
-        switch (m_nextDirection) {
-            case UP:    tryY -= m_speed; break;
-            case DOWN:  tryY += m_speed; break;
-            case LEFT:  tryX -= m_speed; break;
-            case RIGHT: tryX += m_speed; break;
+        switch (nextDir) {
+            case UP:    testY -= m_speed; break;
+            case DOWN:  testY += m_speed; break;
+            case LEFT:  testX -= m_speed; break;
+            case RIGHT: testX += m_speed; break;
+            default: break;
         }
 
-        if (CanMove(tryX, tryY, maze)) {
-            // Snap vào giữa tile và đổi hướng
-            m_x = tileCenterX;
-            m_y = tileCenterY;
-            m_direction = m_nextDirection;
+        if (CanMove(testX, testY, maze)) {
+            currentDir = nextDir;
         }
     }
 
-    // Di chuyển theo hướng hiện tại
+    // Di chuyển theo currentDir
     int newX = m_x, newY = m_y;
 
-    switch (m_direction) {
+    switch (currentDir) {
         case UP:    newY -= m_speed; break;
         case DOWN:  newY += m_speed; break;
         case LEFT:  newX -= m_speed; break;
         case RIGHT: newX += m_speed; break;
+        default: break;
     }
 
     if (CanMove(newX, newY, maze)) {
@@ -103,22 +72,47 @@ void Pacman::Render(SDL_Renderer* renderer) {
     int drawX = m_x - TILE_SIZE / 2;
     int drawY = m_y - TILE_SIZE / 2;
 
+    int row = 0;
+
+    switch (currentDir) {
+        case RIGHT:
+            row = 0;
+            break;
+        case LEFT:
+            row = 1;
+            break;
+        case UP:
+            row = 2;
+            break;
+        case DOWN:
+            row = 3;
+            break;
+    }
+
     TextureManager::GetInstance()->DrawFrame(
-        m_textureID,
-        drawX, drawY,
-        32, 32,         // kích thước frame
-        0, m_frame,     // row, frame
-        SDL_FLIP_NONE
-    );
+    m_textureID,
+    drawX, drawY,
+    32, 32,
+    row, m_frame,
+    SDL_FLIP_NONE
+);
 }
 
 void Pacman::HandleInput(SDL_Event& e) {
     if (e.type == SDL_KEYDOWN) {
-    switch (e.key.keysym.sym) {
-        case SDLK_UP:    m_direction = UP; break;
-        case SDLK_DOWN:  m_direction = DOWN; break;
-        case SDLK_LEFT:  m_direction = LEFT; break;
-        case SDLK_RIGHT: m_direction = RIGHT; break;
+        switch (e.key.keysym.sym) {
+            case SDLK_UP:
+                nextDir = UP;
+                break;
+            case SDLK_DOWN:
+                nextDir = DOWN;
+                break;
+            case SDLK_LEFT:
+                nextDir = LEFT;
+                break;
+            case SDLK_RIGHT:
+                nextDir = RIGHT;
+                break;
+        }
     }
-}
 }
--- BTL-LTNC/src/Object/Pacman.h
+++ BTL-LTNC/src/Object/Pacman.h
@@ -19,8 +19,8 @@ public:
 
 private:
     enum Direction { NONE, UP, DOWN, LEFT, RIGHT };
-    Direction m_direction = DOWN;
-    Direction m_nextDirection = DOWN;
+    Direction currentDir = NONE;
+    Direction nextDir = NONE;
     int m_x, m_y;
     int m_speed ;
 
